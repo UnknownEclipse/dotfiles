268ad1ec83c5b69e31a79a11dbd4401d7a6a3ecb
Eclipse842 <ben.o.friedman@gmail.com>
2024-02-28 14:56:12 -0800

    updated fish config and beets config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,23 @@
+fish_add_path ~/.cargo/bin
+fish_add_path ~/.local/bin
+fish_add_path ~/.pyenv/bin
+
+eval $(/opt/homebrew/bin/brew shellenv)
+
+brew shellenv | source
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    atuin init fish | source
 end
 
-set -x PATH $PATH $HOME/.cargo/bin $HOME/.local/bin
-set -x LS_COLORS $(vivid generate nord)
-set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
-set -x BAT_THEME Nord
-
-alias cat='bat --paging=never'
-alias ls=exa
-alias hx=helix
-alias sudo='sudo '
+set -Ux LS_COLORS $(vivid generate nord)
+set -Ux BAT_THEME "Nord"
+set -Ux MANPAGER "sh -c 'col -bx | bat -l man -p '"
 
 starship init fish | source
 zoxide init fish | source
+pyenv init - | source
+
+alias yt-audio='yt-dlp -x --audio-quality best'
+alias exa='eza'
+alias ls='exa'
