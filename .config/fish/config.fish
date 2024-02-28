18bd782494ba6ffd1b01c39bec23afa0ccca35d1
Eclipse842 <ben.o.friedman@gmail.com>
2024-02-28 15:01:30 -0800

    reorder config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,21 +2,18 @@ fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.pyenv/bin
 
-eval $(/opt/homebrew/bin/brew shellenv)
-
 brew shellenv | source
+pyenv init - | source
 
 if status is-interactive
     atuin init fish | source
+    starship init fish | source
+    zoxide init fish | source
 end
 
 set -Ux LS_COLORS $(vivid generate nord)
 set -Ux BAT_THEME "Nord"
 set -Ux MANPAGER "sh -c 'col -bx | bat -l man -p '"
-
-starship init fish | source
-zoxide init fish | source
-pyenv init - | source
 
 alias yt-audio='yt-dlp -x --audio-quality best'
 alias exa='eza'
